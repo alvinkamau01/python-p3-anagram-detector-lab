be696364065cc0b82028f795ffaf3a301d9594b2
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-09-12 15:02:04 +0300

    Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+# anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        def are_anagrams(word1, word2):
+            return sorted(word1) == sorted(word2)
+        
+        return [word for word in word_list if are_anagrams(self.word, word)]
